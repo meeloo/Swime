d775fef44b2155673963723110f4ea28b7be8ce2
Sébastien Métrot <meeloo@users.noreply.github.com>
2021-09-23 19:02:02 +0200

    update package dependencies

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     .library(name: "Swime", targets: ["Swime"])
   ],
   dependencies: [
-    .package(url: "https://github.com/Quick/Quick", from: "1.3.4"),
-    .package(url: "https://github.com/Quick/Nimble", from: "7.3.3")
+    .package(url: "https://github.com/Quick/Quick", from: "3.1.2"),
+    .package(url: "https://github.com/Quick/Nimble", from: "9.2.1")
   ],
   targets: [
     .target(
